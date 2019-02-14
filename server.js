89b0eeffc5e638f3405a2f84e6e06c3e899605eb
Sowmya Ganesh Gupta <sowmya.bvm@gmail.com>
2019-02-14 13:02:20 -0800

    added tests

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+'use strict';
+
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+if(require.main === module) {
+    app.listen(process.env.PORT || 8080, function(){
+        console.info(`Your app is listening on the port ${this.address().port}`);
+    });
+}
+
+module.exports = app;
---
+++ test/test-server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require("../server.js");
+
+const expect = chai.expect;
+
+describe("index page", function() {
+    it("should exist", function() {
+        return chai
+            .request(app)
+            .get("/")
+            .then(function(res){
+                expect(res).to.have.status(200);
+            });
+    });
+});
